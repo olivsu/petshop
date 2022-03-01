cb810e9c40f0996867b9eb756cf4c813a6ae07d0
Suane Oliveira <oliveiraanara25@gmail.com>
2022-03-01 20:43:20 -0300

    add nodemon no pack json

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var router = express.Router();
-
+const petsController = require("../controller/petsController")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'aula 03/02' });
 });
-router.get('//servico', function(req, res, next) {
-  res.send(modelServico);
-});
+
+//forma errada
+//router.get('/servico', function(req, res, next) {
+  //res.send(modelServico);
+//});
+
+//forma correta
+router.get('/servico', petsController.listaServicos);
+router.get('/pets', petsController.listaPets)
 
 module.exports = router;
